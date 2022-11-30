b361db56712425df92a0f3d68f55fef658f5cc72
rhakbari <rhakbari>
2022-11-30 21:58:17 +0500

    fixes

--- query/fillData.go
+++ query/fillData.go
@@ -22,15 +22,46 @@ func FillDB(db *gorm.DB) error {
 		END $$;`)
 	}
 	if db.Migrator().HasTable("order_items") {
-		db.Exec(`DO $$
-		begin IF EXISTS (select * FROM order_items limit 1) THEN
-		ELSE
-		COPY order_items(id, order_id, price_per_unit, quantity, product)
-		FROM '/tmp/test_data/order_items.csv' 
+		db.Exec(`CREATE TABLE IF NOT EXISTS public.order_items_temp
+		(
+			id text,
+			order_id text,
+			price_per_unit numeric,
+			quantity int4,
+			product text
+		);
+		
+		CREATE OR REPLACE PROCEDURE public.transfer(
+			)
+		LANGUAGE 'plpgsql'
+		AS $BODY$
+		begin
+		
+		copy order_items_temp(id, order_id, price_per_unit, quantity, product)
+		FROM '/tmp/test_data/order_items.csv'
 		DELIMITER ','
 		CSV header;
-		END if;
-		END $$;`)
+		
+		INSERT INTO order_items(id, order_id, price_per_unit, quantity, product)
+			SELECT id, order_id, price_per_unit, quantity, product
+			FROM order_items_temp
+			WHERE not EXISTS (
+				SELECT id, order_id, price_per_unit, quantity, product
+				FROM order_items
+				WHERE order_items_temp.id = order_items.id);
+				commit;
+		
+		truncate table order_items_temp;
+		end;
+		$BODY$;
+		ALTER PROCEDURE public.transfer()
+			OWNER TO postgres;
+		
+		CALL public.transfer();
+		
+		select * from order_items;
+		
+		select * from order_items_temp;`)
 	}
 	if db.Migrator().HasTable("orders") {
 
@@ -54,3 +85,56 @@ func FillDB(db *gorm.DB) error {
 
 	return nil
 }
+
+// DO $$
+// 		begin IF EXISTS (select * FROM order_items limit 1) THEN
+// 		ELSE
+// 		COPY order_items(id, order_id, price_per_unit, quantity, product)
+// 		FROM '/tmp/test_data/order_items.csv'
+// 		DELIMITER ','
+// 		CSV header;
+// 		END if;
+// 		END $$;
+/*
+CREATE TABLE IF NOT EXISTS public.order_items_temp
+(
+    id text,
+    order_id text,
+    price_per_unit numeric,
+    quantity int4,
+    product text
+);
+
+CREATE OR REPLACE PROCEDURE public.transfer(
+	)
+LANGUAGE 'plpgsql'
+AS $BODY$
+begin
+
+copy order_items_temp(id, order_id, price_per_unit, quantity, product)
+FROM '/tmp/test_data/order_items.csv'
+DELIMITER ','
+CSV header;
+
+INSERT INTO order_items(id, order_id, price_per_unit, quantity, product)
+    SELECT id, order_id, price_per_unit, quantity, product
+    FROM order_items_temp
+    WHERE not EXISTS (
+        SELECT id, order_id, price_per_unit, quantity, product
+        FROM order_items
+        WHERE order_items_temp.id = order_items.id);
+		commit;
+
+truncate table order_items_temp;
+end;
+$BODY$;
+ALTER PROCEDURE public.transfer()
+    OWNER TO postgres;
+
+CALL public.transfer();
+
+select * from order_items;
+
+select * from order_items_temp;
+
+*/
